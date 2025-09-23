d2dc0ba1e88c912d0f45178e63af9b53f8bdc91a
Joey Rudd <emailjoeyrudd@gmail.com>
2025-09-23 13:45:20 -0400

    Add JUnit4 to module path

---
+++ src/ca/yorku/eecs3311/assignment1/othello/PlayerRandom.java
@@ -0,0 +1,19 @@
+package ca.yorku.eecs3311.assignment1.othello;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * PlayerRandom makes a move by first determining all possible moves that this
+ * player can make, putting them in an ArrayList, and then randomly choosing one
+ * of them.
+ *
+ */
+public class PlayerRandom {
+	
+	private Random rand = new Random();
+
+	public Move getMove() {
+		return null;
+	}
+}
